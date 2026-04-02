d2ab75e0dcdb9dd0d101619aa5ff008cdce19d6a
Syed Rabiaan <syedrabiaan@gmail.com>
2026-04-02 12:45:20 +0500

    Add scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -9,6 +10,7 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <div className="min-h-screen bg-white selection:bg-brand-blue selection:text-white overflow-x-hidden">
+      <ScrollToTop />
       <Navbar />
       <main>
         <Routes>
